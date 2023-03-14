634942bd7b8a05c00c99241f5ba8199679adec5f
Ben Buchanan <benbuchanan11@yahoo.com>
2023-03-14 15:49:46 -0400

    Initial Commit

---
+++ ScribbleAI/ScribbleAIApp.swift
@@ -0,0 +1,20 @@
+//
+//  ScribbleAIApp.swift
+//  ScribbleAI
+//
+//  Created by Ben Buchanan on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ScribbleAIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
